5c232c6a8dc37f61eb053c8536840d0b68e904da
avgx <agovorovsky@gmail.com>
2026-05-02 09:03:11 +0300

    NoopURLSessionTaskLogger

---
+++ Sources/DebugThings/Network/NoopURLSessionTaskLogger.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public struct NoopURLSessionTaskLogger: URLSessionTaskLogger {
+    public func logTaskCreated(_ task: URLSessionTask) {}
+    public func logTask(_ task: URLSessionTask, didCompleteWithError error: Error?) {}
+    public func logTask(_ task: URLSessionTask, didFinishCollecting metrics: URLSessionTaskMetrics) {}
+    public func logTask(_ task: URLSessionTask, didFinishDecodingWithError error: Error?) {}
+    public func logDataTask(_ dataTask: URLSessionDataTask, didReceive data: Data) {}
+}
